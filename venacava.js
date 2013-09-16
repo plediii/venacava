4e18dc199fcdc374f7896108269432b2102a9439
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2013-09-16 20:48:03 +0000

    Add a model class satisfying the instance tests.

--- venacava.js
+++ venacava.js
@@ -290,6 +290,35 @@ _.extend(Core.prototype, {
     }
 });
 
+var Model = exports.Model = function (redis, proto) {
+    var _this = this
+    ;
+    _this._redis = redis
+    _this.defaults = {}
+    _this.Klass = function (core) {
+	this.core = core;
+    }
+    _this.defaults = proto.defaults;
+
+    _.extend(_this.Klass.prototype, proto);
+};
+
+_.extend(Model.prototype, {
+    create: function (attrs) {
+	var _this = this
+	;
+	attrs = _.defaults(_.clone(attrs), _this.defaults);
+	var instance = _this.get(random_string());
+	instance.core.set(attrs);
+	instance.initialize && instance.initialize();
+	return instance;	
+    }
+    , get: function (channel) {
+	var _this = this;
+	return new _this.Klass(new Core(channel, _this._redis));	
+    }
+});
+
 var printExec = function (err, replies) {
     log('exec err = ', err);
     if (replies) {
