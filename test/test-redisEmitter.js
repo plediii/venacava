2d2794a17676d02a490f287e88bdfe0e354f7452
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2013-09-22 03:07:05 +0000

    Add preliminary redisEmitter tests.

---
+++ test/test-redisEmitter.js
@@ -0,0 +1,209 @@
+
+var venacava = require('../venacava.js')
+, RedisEmitter = venacava.RedisEmitter
+, redis = require('redis')
+, assert = require('assert')
+, _ = require('underscore')
+;
+
+var redisClient = function () {
+    return redis.createClient();
+};
+
+var randomId = function () {
+    return 'test' + Math.floor(Math.random() * 1000000);
+};
+
+var MockRedisSub = function () {
+    this.subscriptions = {};
+    this.messageListeners = [];
+    this.subscriptionListeners = [];
+};
+
+_.extend(MockRedisSub.prototype, {
+    subscribe: function (channel) {
+	var _this = this;
+	_this.subscriptions[channel] = 1 + (_this.subscriptions[channel] || 0);
+	_.each(_this.subscribeListeners, function (listener) {
+	    listener(channel, _.keys(_this.subscriptions).length);
+	});
+    }
+    , unsubscribe: function (channel) {
+	delete this.subscriptions[channel];
+    }
+    , on: function (type, listener) {
+	if (type === 'message') {
+	    this.messageListeners.push(listener);
+	}
+	else if (type === 'subscribe') {
+	    this.subscribeListeners.push(listener);
+	}
+    }
+    , _emit: function (channel, message) {
+	if (this.subscriptions[channel] > 0) {
+	    _.each(this.messageListeners, function (listener) {
+		listener(channel, message);
+	    });
+	}
+    }
+});
+
+describe('RedisEmitter', function () {
+
+    var redis
+    ;
+
+    var newRemitter = function (theRedis) {
+	theRedis = theRedis || redis;
+	return new RedisEmitter(redis);
+    };
+
+
+
+    before(function(done) {
+	redis = redisClient();
+	return done();
+    });
+
+    it('should be constructable', function () {
+	newRemitter(redis);
+    });
+
+    it('should subscribe to new channels on the redis subscriber', function () {
+	var channel = randomId()
+	, redisSub = new MockRedisSub()
+	, remitter = newRemitter(redisSub)
+	;
+	
+	remitter.subscribe(channel);
+	assert.equal(redisSub.subscriptions[channel], 1);
+    });
+
+
+    it('should only subscribe to new channels on the redis subscriber for the first listener', function () {
+	var channel = randomId()
+	, redisSub = new MockRedisSub()
+	, remitter = newRemitter(redisSub)
+	;
+	
+	remitter.subscribe(channel, function () {});
+	remitter.subscribe(channel, function () {});
+	assert.equal(redisSub.subscriptions[channel], 1);
+    });
+
+    it('should unsubscribe on the redis subscriber when the listener unsubscribes', function () {
+	var channel = randomId()
+	, redisSub = new MockRedisSub()
+	, remitter = newRemitter(redisSub)
+	, listener = function () {}
+	;
+	
+	remitter.subscribe(channel, listener);
+	remitter.unsubscribe(channel, listener)
+	assert(!redisSub.hasOwnProperty(channel));
+    });
+
+    it('should not unsubscribe when there are remaining listeners', function () {
+	var channel = randomId()
+	, redisSub = new MockRedisSub()
+	, remitter = newRemitter(redisSub)
+	, listener = function () {}
+	;
+	
+	remitter.subscribe(channel, listener);
+	remitter.subscribe(channel, function () {});
+	remitter.unsubscribe(channel, listener);
+	assert.equal(redisSub.subscriptions[channel], 1);
+    });
+
+    it('should unsubscribe when all listeners have unsubscribed', function () {
+	var channel = randomId()
+	, redisSub = new MockRedisSub()
+	, remitter = newRemitter(redisSub)
+	, listener = function () {}
+	, listener2 = function () {}
+	;
+	
+	remitter.subscribe(channel, listener);
+	remitter.subscribe(channel, listener2);
+	remitter.unsubscribe(channel, listener);
+	remitter.unsubscribe(channel, listener2);
+	assert(!redisSub.hasOwnProperty(channel));
+    });
+
+    it('should relay messages on subscribed channel to listener', function () {
+
+	var channelA = randomId()
+	, channelB = randomId()
+	, redisSub = new MockRedisSub()
+	, remitter = newRemitter(redisSub)
+	, messages = []
+	, listenA = function (msg) {
+	    messages.push(['a', msg]);
+	}
+	, countB = 0
+	, listenB = function () {
+	    messages.push(['b', msg]);
+	}
+	;
+	remitter.subscribe(channelA, listenA);
+	remitter.subscribe(channelB, listenB);
+	redisSub._emit(channelA, 1);
+	assert.deepEqual(messages, [['a', 1]]);
+    });
+
+    it('should relay messages on subscribed channel to all listeners', function () {
+
+	var channelA = randomId()
+	, channelB = randomId()
+	, redisSub = new MockRedisSub()
+	, remitter = newRemitter(redisSub)
+	, messages = []
+	, listenA = function (msg) {
+	    messages.push(['a', msg]);
+	}
+	, countB = 0
+	, listenB = function () {
+	    messages.push(['b', msg]);
+	}
+	;
+	remitter.subscribe(channelA, listenA);
+	remitter.subscribe(channelA, listenB);
+	redisSub._emit(channelA, 1);
+	assert.equal(1, _.countBy(messages, function (elt) {
+	    return elt[0] === 'a' && elt[1] === 1;
+	}));
+	assert.equal(1, _.countBy(messages, function (elt) {
+	    return elt[0] === 'b' && elt[1] === 1;
+	}));
+    });
+
+    it('should not relay messages to unsubscribed listeners', function () {
+
+	var channelA = randomId()
+	, channelB = randomId()
+	, redisSub = new MockRedisSub()
+	, remitter = newRemitter(redisSub)
+	, messages = []
+	, listenA = function (msg) {
+	    messages.push(['a', msg]);
+	}
+	, countB = 0
+	, listenB = function () {
+	    messages.push(['b', msg]);
+	}
+	;
+	remitter.subscribe(channelA, listenA);
+	remitter.subscribe(channelA, listenB);
+	remitter.unsubscribe(channelA, listenB);
+	redisSub._emit(channelA, 1);
+	assert.equal(1, _.countBy(messages, function (elt) {
+	    return elt[0] === 'a' && elt[1] === 1;
+	}));
+	assert.equal(0, _.countBy(messages, function (elt) {
+	    return elt[0] === 'b' && elt[1] === 1;
+	}));
+    });
+
+});
+      
