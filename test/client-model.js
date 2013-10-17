d67826fc649a79338743fbe402ed00a1ff8c232d
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2013-10-17 00:45:30 +0000

    In client-model tests, expect instances to be service instances which contain the models.

--- test/client-model.js
+++ test/client-model.js
@@ -4,14 +4,14 @@ var venaclient = require('../venaclient.js')
 , EventEmitter = require('events').EventEmitter
 , _ = require('underscore')
 , Backbone = require('backbone')
-, MockSocet = require('mocksocket').MockSocket
+, MockSocket = require(__dirname + '/mocksocket').MockSocket
 ;
 
 var randomId = function () {
     return 'test' + Math.floor(Math.random() * 1000000);
 };
 
-var newContext = function () {
+var newContext = function (options) {
     var name = 'test'
     , Model = Backbone.Model.extend({
 	urlRoot: name
@@ -26,7 +26,7 @@ var newContext = function () {
 	, name: name
 	, socket: socket
 	, client: client
-	, service: new client.Service(name, Model)
+	, service: new client.Service(name, Model, options)
     };
 }
 ;
@@ -56,196 +56,224 @@ describe('venaclient', function () {
 	    });
 
 	    describe(':instance', function () {
-		it('should be of the expected type', function () {
-		    var context = newContext();
-		    assert(context.service.get(randomId()) instanceof context.Model);
+
+		it('should have the instance channel', function () {
+		    var channel = randomId();
+		    assert.equal(newContext().service.get(channel).channel, channel);
+		});
+
+		it('should always be the same object for the same channel', function () {
+		    var context = newContext()
+		    , channel = randomId()
+		    , instance = context.service.get(channel)
+		    , otherInstance = context.service.get(channel)
+		    ;
+		    instance.x = 1;
+		    otherInstance.x = 2;
+		    assert.equal(otherInstance.x, instance.x);
+		});
+
+		describe('#socket', function () {
+		    it('should exist', function () {
+			var context = newContext();
+			assert(_.isObject(context.service.get(randomId()).socket));
+		    });
+
+		    it('should be the service socket', function () {
+			var context = newContext();
+			assert.equal(context.service.get(randomId()).socket, context.socket);
+		    });
 		});
 
 
-		describe('#service', function () {
+		describe('#model', function () {
 
 		    it('should exist', function () {
 			var context = newContext();
-			assert(_.isOjbect(context.service.get(randomId()).service));
+			assert(_.isObject(context.service.get(randomId()).model));
 		    });
 
-		    it('should have the instance channel', function () {
-			var channel = randomId();
-			assert.equal(newcontext().service.get(channel).service.channel, channel);
+		    it('should be of the expected type', function () {
+			var context = newContext();
+			assert(context.service.get(randomId()).model instanceof context.Model);
 		    });
 
-		    describe('#socket', function () {
-			it('should exist', function () {
-			    var context = newContext();
-			    assert(_.isOjbect(context.service.get(randomId()).service.socket));
-			});
+		});
 
-			it('should be the service socket', function () {
-			    var context = newContext();
-			    assert.equal(context.service.get(randomId()).service.socket, context.socket);
-			});
+		describe('#subscribe', function () {
+		    it('should exist', function () {
+			var context = newContext();
+			assert.equal(typeof context.service.get(randomId()).subscribe, 'function');
 		    });
 
-		    describe('#subscribe', function () {
-			it('should exist', function () {
-			    var context = newContext();
-			    assert.equal(typeof context.service.get(randomId()).service.subscribe, 'function');
-			});
-
-			it('should trigger "set" updates on the model', function (done) {
-			    var context = newContext()
-			    , channel = randomId()
-			    , instance = context.service.get(channel)
-			    ;
-			    instance.service.subscribe();
-			    instance.on('change:x', function () {
-				assert.equal(instance.get('x'), 1);
-				done();
-			    })
-			    context.socket._receive(instance.service.channel, {
-				subject: 'set'
-				, body: {
-				    x: 1
-				}
-			    });
-			});
-
-			it('should trigger "unset" updates on the model', function (done) {
-			    var context = newContext()
-			    , channel = randomId()
-			    , instance = context.service.get(channel)
-			    ;
-			    instance.service.subscribe();
-			    instance.on('change:x', function () {
-				assert(!instance.has('x'));
-				done();
-			    })
-			    context.socket._receive(instance.service.channel, {
-				subject: 'unset'
-				, body: {
-				    x: 1
-				}
-			    });
-			});
-
-			
-		    });
-
-		    describe('#unsubscribe', function () {
-			it('should exist', function () {
-			    var context = newContext();
-			    assert.equal(typeof context.service.get(randomId()).service.subscribe, 'function');
-			});
-
-			it('should trigger "unset" updates on the model', function () {
-			    var context = newContext()
-			    , channel = randomId()
-			    , instance = context.service.get(channel)
-			    , called = 0
-			    ;
-			    instance.service.subscribe();
-			    instance.on('change:x', function () {
-				called = called + 1
-			    });
-			    context.socket._receive(instance.service.channel, {
-				subject: 'set'
-				, body: {
-				    x: 1
-				}
-			    });
-			    instance.service.unsubscribe();
-			    context.socket._receive(instance.service.channel, {
-				subject: 'set'
-				, body: {
-				    x: 2
-				}
-			    });
-			    assert.equal(called, 1);
-			});
-		    });
-
-		    describe(':methods', function () {
-			
-			it('should exist', function () {
-			    var context = newContext({
-				methods: ['method']
-			    });
-			    assert.equal(typeof context.service.get(randomId()).service.method, 'function');
-			});
-
-			it('should transmit the method over the socket', function (done) {
-			    var context = newContext({
-				methods: ['method']
-			    })
-			    , instance = context.service.get(randomId())
-			    ;
-			    context.socket._emit(instance.service.name, function (msg) {
-				assert.equal(msg.channel, instance.service.channel);
-				assert.equal(msg.method, 'method');
-				assert(msg.hasOwnProperty('data'), 'service method invocation msg did not have a data property.')
-				assert.equal(msg.data.x, 1);
-			    });
-			    instance.serivce.method({
+		    it('should trigger "set" updates on the model', function (done) {
+			var context = newContext()
+			, channel = randomId()
+			, instance = context.service.get(channel)
+			;
+			instance.subscribe();
+			instance.model.on('change:x', function () {
+			    assert.equal(instance.model.get('x'), 1);
+			    done();
+			})
+			context.socket._receive(instance.channel, {
+			    subject: 'set'
+			    , body: {
 				x: 1
-			    });
+			    }
+			});
+		    });
+
+		    it('should trigger "unset" updates on the model', function (done) {
+			var context = newContext()
+			, channel = randomId()
+			, instance = context.service.get(channel)
+			;
+			instance.subscribe();
+			instance.model.set('x', 1);
+			instance.model.on('change:x', function () {
+			    assert(!instance.model.has('x'));
+			    done();
+			})
+			context.socket._receive(instance.channel, {
+			    subject: 'unset'
+			    , body: {
+				x: 1
+			    }
+			});
+		    });
+
+		    
+		});
+
+		describe('#unsubscribe', function () {
+		    it('should exist', function () {
+			var context = newContext();
+			assert.equal(typeof context.service.get(randomId()).subscribe, 'function');
+		    });
+
+		    it('should not trigger "set" updates on the model afterward', function () {
+			var context = newContext()
+			, channel = randomId()
+			, instance = context.service.get(channel)
+			, called = 0
+			;
+			instance.subscribe();
+			instance.model.on('change:x', function () {
+			    called = called + 1
+			});
+			context.socket._receive(instance.channel, {
+			    subject: 'set'
+			    , body: {
+				x: 1
+			    }
+			});
+			instance.unsubscribe();
+			context.socket._receive(instance.channel, {
+			    subject: 'set'
+			    , body: {
+				x: 2
+			    }
+			});
+			assert.equal(called, 1);
+		    });
+		});
+
+		describe(':methods', function () {
+		    
+		    it('should exist', function () {
+			var context = newContext({
+			    methods: ['method']
+			});
+			assert.equal(typeof context.service.get(randomId()).method, 'function');
+		    });
+
+		    it('should transmit the method over the socket', function (done) {
+			var context = newContext({
+			    methods: ['method']
+			})
+			, instance = context.service.get(randomId())
+			;
+			context.socket._emit(context.service.name, function (msg) {
+			    assert(msg);
+			    assert.equal(msg.channel, instance.channel);
+			    assert.equal(msg.method, 'method');
+			    assert(msg.hasOwnProperty('data'), 'service method invocation msg did not have a data property.')
+			    assert.equal(msg.data.x, 1);
+			    done()
+			});
+			instance.method({
+			    x: 1
+			});
+		    });
+
+		});
+
+		describe('#on', function () {
+		    
+		    it('should be triggered by socket events for the target channel', function (done) {
+			var context = newContext({
+			    methods: ['method']
+			})
+			, instance = context.service.get(randomId())
+			;
+			instance.subscribe();
+			instance.model.on('trigger', function (data) {
+			    done();
+			});
+
+			context.socket._receive(instance.channel, {
+			    subject: 'trigger'
+			    , trigger: 'trigger'
+			});
+		    });
+
+		    it('should be triggered with data', function (done) {
+			var context = newContext({
+			    methods: ['method']
+			})
+			, instance = context.service.get(randomId())
+			;
+			instance.subscribe();
+			instance.model.on('trigger', function (data) {
+			    assert.equal(data.x, 1);
+			    done();
+			});
+
+			context.socket._receive(instance.channel, {
+			    subject: 'trigger'
+			    , trigger: 'trigger'
+			    , data: {
+				x: 1
+			    }
+			});
+		    });
+
+		    it('should not be triggered by socket events for other channels', function () {
+			var context = newContext({
+			    methods: ['method']
+			})
+			, instance = context.service.get(randomId())
+			, called = 0
+			;
+			instance.subscribe();
+			instance.model.on('trigger', function (data) {
+			    called = called + 1;
+			});
+
+			context.socket._receive(randomId(), {
+			    subject: 'trigger'
+			    , trigger: 'trigger'
+			});
+
+			context.socket._receive(instance.channel, {
+			    subject: 'trigger'
+			    , trigger: 'trigger'
 			});
 
 		    });
 
-		    describe('#on', function () {
-			
-			it('should be triggered by socket events for the target channel', function (done) {
-			    var context = newContext({
-				methods: ['method']
-			    })
-			    , instance = context.service.get(randomId())
-			    ;
-			    instance.service.on('trigger', function (data) {
-				done();
-			    });
 
-			    context.socket._receive(instance.service.channel, {
-				trigger: 'trigger'
-			    });
-			});
-
-			it('should be triggered with data', function (done) {
-			    var context = newContext({
-				methods: ['method']
-			    })
-			    , instance = context.service.get(randomId())
-			    ;
-			    instance.service.on('trigger', function (data) {
-				asser.equal(data.x, 1);
-				done();
-			    });
-
-			    context.socket._receive(instance.service.channel, {
-				trigger: 'trigger'
-				, data: {
-				    x: 1
-				}
-			    });
-			});
-
-			it('should not be triggered by socket events for other channels', function (done) {
-			    var context = newContext({
-				methods: ['method']
-			    })
-			    , instance = context.service.get(randomId())
-			    ;
-			    instance.service.on('trigger', function (data) {
-				done();
-			    });
-
-			    context.socket._receive(randomId(), {
-				trigger: 'trigger'
-			    });
-			});
-
-
-
-		    });
 
 		});
 
