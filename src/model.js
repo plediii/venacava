33cba85780328eeef0e74c429d960cbd8e81307c
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2013-10-29 03:38:46 +0000

    Update model to pass new tests.

--- src/model.js
+++ src/model.js
@@ -4,18 +4,20 @@ var _ = require('underscore')
 , random_string = require(__dirname + '/util').random_string
 ;
 
-var Model = exports.Model = function (options) {
+var Model = exports.Model = function (name, options) {
     var _this = this
-    , Klass = function (core) {
+    , Klass = function (channel, id, core) {
+	this.id = id;
+	this.channel = channel;
 	this.core = core;
     }
     ;
+    _this.name = name;
 
     options = _.defaults({}, options, {
 	methods: {}
 	, initialize: function () {}
 	, defaults: {}
-	, channelRoot: ''
 	, Core: Core
     });
     _.extend(Klass.prototype, options.methods);
@@ -24,18 +26,58 @@ var Model = exports.Model = function (options) {
     });
 };
 
+var getChannel = function (name, id) {
+    return name + '/' + id;
+};
+
+var initialize = function (instance, initializer, attrs, options) {
+    instance.core.set(attrs);
+    // instance.core.set({id: instance.id})
+    initializer.call(instance);
+    return instance;
+};
+
 _.extend(Model.prototype, {
     create: function (attrs, options) {
 	var _this = this
+	, id = random_string()
 	;
 	attrs = _.defaults({}, attrs, _this.defaults);
-	var instance = _this.get(_this.channelRoot + random_string(), attrs, options);
-	instance.core.set(attrs);
-	_this.initialize.call(instance);
-	return instance;	
+
+	return initialize(_this.get(id, attrs, options)
+		   , _this.initialize, attrs, options)
     }
-    , get: function (channel, attrs, options) {
-	var _this = this;
-	return new _this.Klass(new _this.Core(channel, attrs, options));
+    , get: function (id, attrs, options) {
+	var _this = this
+	, channel = getChannel(_this.name, id)
+	;
+	return new _this.Klass(channel, id, new _this.Core(channel, attrs, options));
+    }
+    , createIfNotExists: function (id, attrs, options, cb) {
+	var _this = this
+	, instance = _this.get(id, attrs, options);
+	;
+	if (_.isFunction(options)) {
+	    cb = options;
+	    options = void 0;
+	}
+	else if (_.isFunction(attrs)) {
+	    cb = attrs;
+	    attrs = options = void 0;
+	}
+
+	return instance.core.exists(function (err, exists) {
+	    if (err) {
+		return cb(err);
+	    }
+	    else {
+		if (!exists) {
+		    return cb(null, initialize(instance, _this.initialize, attrs, options));
+		}
+		else {
+		    return cb(null, instance);
+		}
+	    }
+	});
     }
 });
