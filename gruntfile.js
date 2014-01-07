0647ef2112efdb4d441cab090e7b9e7aa39e4996
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2014-01-07 04:32:55 +0000

    Add grunt dependency and gruntfile.

---
+++ gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	jshint: {
+            options: {
+		laxcomma: true
+		// , smarttabs: true
+            }
+            , all: [ '*.js', 'src/*.js']
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Default task(s).
+    grunt.registerTask('default', ['jshint']);
+
+};
